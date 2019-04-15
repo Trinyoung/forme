420d419ad287cea980511e870fe7df8c7f190bd9
trinyoung <trinyoung@163.com>
2019-04-15 23:42:56 +0800

    feature(structure): "fix all"

---
+++ stores/Homestore.js
@@ -0,0 +1,21 @@
+import alt from '../alt';
+import HomeActions from '../actions/HomeActions';
+
+class HomeStore {
+  constructor() {
+    this.bindActions(HomeActions);
+    this.characters = [];
+  }
+
+  onGetTwoCharactersSuccess(data) {
+    this.characters = data;
+  }
+
+  onGetTwoCharactersFail(errorMessage) {
+    toastr.error(errorMessage);
+  }
+
+  onVoteFail(errorMessage) {
+    toastr.error(errorMessage);
+  }
+}
